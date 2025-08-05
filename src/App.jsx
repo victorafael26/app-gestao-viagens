529af984e4eb032f392a6f38eeca5ea99e62108a
victorafael26 <vrafael2601@gmail.com>
2025-08-05 11:53:15 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,13 @@
+// src/App.jsx
+import React from 'react'
 
+function App() {
+  return (
+    <div>
+      <h1>🚀 App Gestão de Viagens</h1>
+      <p>Deploy com Supabase + Vercel funcionando!</p>
+    </div>
+  )
+}
+
+export default App
