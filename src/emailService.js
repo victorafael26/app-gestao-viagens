f44bd18d225b915e5df75234c000228d0050391a
victorafael26 <vrafael2601@gmail.com>
2025-08-05 13:44:21 -0300

    Update emailService.js

--- src/emailService.js
+++ src/emailService.js
@@ -1,11 +1,11 @@
 // src/emailService.js
 import emailjs from '@emailjs/browser'
 
-export const sendNotification = async (templateData) => {
+export const sendNotification = async (templateId, templateData) => {
   try {
     await emailjs.send(
       import.meta.env.VITE_EMAILJS_SERVICE_ID,
-      import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+      templateId,
       templateData,
       import.meta.env.VITE_EMAILJS_PUBLIC_KEY
     )
